1c5b4083f21c4614f1047ee5817f9507d5370b10
Juan Morales Martinez <juanmoralesmartinez@Juans-MacBook-Pro.local>
2023-11-24 18:21:06 +0100

    done

--- index.js
+++ index.js
@@ -48,3 +48,47 @@ else{
 }
 
 
+    
+// Bonus 1: number of words and `et`
+
+const paragraph = `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque id ante mollis, sagittis dolor sit amet, tempus orci. Vestibulum cursus luctus tellus, et scelerisque eros porta ullamcorper. Mauris quis ullamcorper elit. Nullam nulla mauris, tempus eu urna nec, faucibus tristique massa. Sed id augue in augue sollicitudin dictum. Fusce maximus eget velit ut fringilla. Praesent consectetur et placerat egestas. Suspendisse auctor ligula urna, a pretium magna bibendum pretium. Nulla aliquet turpis et eros luctus tristique. Nunc efficitur dictum lectus. Duis eu arcu ipsum. Phasellus ipsum nulla, dignissim quis commodo quis, efficitur eget elit. Pellentesque convallis dolor eget aliquet elementum. Nunc libero erat, facilisis consectetur ornare sagittis, luctus sed eros.
+
+Curabitur varius turpis et eros ullamcorper faucibus. Nulla id dignissim orci. Suspendisse quis blandit leo. Pellentesque pellentesque libero dapibus nisl tincidunt fermentum. Donec sapien ipsum, pellentesque sit amet mauris sed, viverra viverra massa. Vestibulum ante erat, ultrices eget diam id, vehicula laoreet diam. Aliquam at suscipit sapien, et accumsan nulla. Nulla ut augue sed libero venenatis et sollicitudin. Nam eu ligula ultricies, sagittis eros vel, egestas ipsum. In magna ante, iaculis et porta non, eleifend condimentum ante. Phasellus eget purus pharetra, ultrices nisi vel, rutrum mi. Proin sem diam, convallis quis consequat nec, faucibus nec metus. Praesent scelerisque est ut arcu pellentesque tempor. Pellentesque faucibus viverra sagittis. Aenean in lacinia massa. In hac habitasse platea dictumst.
+
+Sed hendrerit, metus eget blandit vulputate, orci urna rhoncus lacus, tempor fermentum libero magna in odio. Morbi elit arcu, sodales sed ex sit amet, lacinia ullamcorper nunc. Proin eu justo consequat, interdum ligula eu, porttitor arcu. Morbi tempor purus auctor orci lobortis iaculis. Aliquam lacinia laoreet mollis. Pellentesque dapibus non arcu id eleifend. Praesent scelerisque pellentesque ligula. Sed ac et ultrices mauris.`
+
+
+let words = 0;
+let etCount = 0;
+
+
+let splittedParagraph = paragraph.split(' ');
+
+for (let i=0; i < splittedParagraph.length; i++) {
+  if(splittedParagraph[i].includes('\n')) {
+    words++
+  } else if(splittedParagraph[i] === "et" || splittedParagraph[i] === "et.") {
+    etCount++;
+  }
+  words++;
+ }
+
+console.log('words -->', words);
+console.log('et count  -->', etCount);
+
+
+// Bonus 2: Palindrome
+
+let stringToCheck = "A man, a plan, a canal, Panama!";
+stringToCheck = stringToCheck.replace(/[ ,!.]/g, "").toLowerCase();
+
+let reversedString = stringToCheck.split('').reverse().join('');
+
+if (stringToCheck === reversedString) {
+  console.log('String is a palindrome');
+}
+else {
+  console.log('Not a palindrome');
+}
+
+
